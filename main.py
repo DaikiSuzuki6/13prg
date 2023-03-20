5bc09354111ab3e847285cb799005644a76dac68
156106 <156106@cloud.rangitoto.school.nz>
2023-03-21 10:00:40 +1300

    For this session, the space bar did not seem to register once the bird has flapped once. To debug this issue, I realised that the while loop was repeating without taking account of the space button. So I added a line which resets the velocity of the flap each time it is pressed, and a self.alive to ensure that the loop is repeating if the bird has not bumped into the pipe

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
-# Import tkinter
 from tkinter import *
+import random
 
-SCREEN_HEIGHT = 720
-SCREEN_WIDTH = 1280
-GRAVITY = 9.81  # Adjust this value to change the strength of gravity
-
-
-# Create Bird
 class Bird:
     """Create a bird for user to control"""
 
-    # Height of jump when space is pressed
-    JUMP_HEIGHT = 20
+    time_step = 0.01  # Time step in seconds
+    gravity_pull = 1900.81  # Gravitational acceleration in m/s^2
 
-    def __init__(self, canvas):
+    def __init__(self, master, v_init, y_init, size, canvas):
+        self.master = master
         self.canvas = canvas
-        self.id = canvas.create_rectangle(150, SCREEN_HEIGHT // 2 - 10, 170, SCREEN_HEIGHT // 2 + 10, fill="red")
-        self.velocity = 0
+        self.v_init = v_init
+        self.y_init = y_init
+        self.size = size
         self.alive = True
-        self.coords = self.canvas.coords(self.id)
 
-    def update(self):
-        # Update the bird's position based on its velocity
-        self.velocity += GRAVITY
-        self.canvas.move(self.id, 0, self.velocity)
-        self.coords = self.canvas.coords(self.id)
+        self.bird = self.canvas.create_rectangle(200 - self.size / 2,
+                                                   720 - self.y_init - self.size / 2,
+                                                   200 + self.size / 2,
+                                                   720 - self.y_init + self.size / 2,
+                                                    fill='red')
 
-        # Keep the bird from going off the bottom of the screen
-        if self.coords[3] >= SCREEN_HEIGHT:
-            self.velocity = 0
-            self.canvas.move(self.id, 0, SCREEN_HEIGHT - self.coords[3])
+        # Bind the space key to jump
+        self.master.bind_all("<space>", self.bird_jump)
+
+    def bird_jump(self, event):
+
+        # Reset the initial velocity going up each space press
+        self.v_init = 500
+
+        while self.y_init > 0 and self.alive:
+            self.y_init = self.y_init + self.v_init * self.time_step
+            self.v_init = self.v_init - self.gravity_pull * self.time_step
+
+            if self.y_init < self.size:
+                self.y_init = self.size
+
+            if self.y_init > 720 - self.size:
+                self.y_init = 720 - self.size
+                self.v_init = 0
+
+            self.canvas.coords(self.bird, 200 - self.size / 2,
+                               720 - self.y_init - self.size / 2,
+                               200 + self.size / 2, 720 - self.y_init + self.size / 2)
+
+            self.master.update()
+
+            self.canvas.after(10)
 
     def reset(self):
         """Reset the bird to its original position"""
         pass
 
 
-
 class Pipes:
-    def __init__(self):
+    def __init__(self, distance):
         pass
+
 
 
 class Score:
@@ -48,25 +65,21 @@ class Score:
 
 
 class Game:
+    SCREEN_HEIGHT = 720
+    SCREEN_WIDTH = 1280
+
     def __init__(self):
         self.main_window = Tk()
         self.main_window.title('Flappy Bird')
-        self.canvas = Canvas(self.main_window, width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+
+        self.canvas = Canvas(self.main_window, width=Game.SCREEN_WIDTH, height=Game.SCREEN_HEIGHT)
         self.canvas.pack()
 
-        # Create the bird
-        self.bird = Bird(self.canvas)
-
-        # Bind the space key to jump
-        self.canvas.bind_all('<space>', self.bird_jump)
-
-    def bird_jump(self, event):
-        # Make the bird jump when the space key is pressed
-        self.bird.velocity = -Bird.JUMP_HEIGHT
+        # Initialise the Jump function
+        self.bird = Bird(self.main_window, canvas=self.canvas, v_init=500, y_init=360, size=20)
 
     def start(self):
         while True:
-            self.bird.update()
             self.main_window.update_idletasks()
             self.main_window.update()
 
